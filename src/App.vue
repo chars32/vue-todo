4b402754048d967b5a7e890d3685f20e3c6891bc
Carlos Garcia <chars24@gmail.com>
2017-11-25 21:07:28 -0600

    1. Setup y haciendo el menu

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <app-header></app-header>
+  </v-app>
+</template>
+
+<script>
+  import Header from './components/Header.vue'
+
+  export default {
+    components: {
+      appHeader: Header
+    }
+  }
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-layout wrap>
+    <!-- menu lateral -->
+    <v-container>
+      <v-layout row wrap>
+        <v-flex xs12>
+          <v-card>
+            <v-toolbar color="white">
+              <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+              <h3>Todo List</h3>
+            </v-toolbar>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <v-navigation-drawer
+      temporary
+      v-model="drawer"
+      light
+      absolute
+    >
+      <v-list class="pa-3">
+        <h1 class="text-xs-center">Todo List</h1>
+      </v-list>
+      <v-list class="pt-0" dense>
+        <v-divider></v-divider>
+        <v-list-tile class="pt-1" v-for="item in items" :key="item.title" @click="">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <!-- termina menu lateral -->
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        drawer: null,
+        items: [
+          { title: 'Home', icon: 'dashboard' },
+          { title: 'About', icon: 'question_answer' }
+        ]
+      }
+    }
+  }
+</script>
